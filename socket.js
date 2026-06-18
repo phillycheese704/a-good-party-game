46aaf5f3506b2b60f53bff368b28736039b41b3e
phillycheese704 <phillycheese704@gmail.com>
2026-06-18 19:47:35 +0100

    Delete socket.js

--- socket.js
+++
@@ -1,7 +0,0 @@
-import { io } from "socket.io-client";
-
-const serverUrl = window.location.hostname === "localhost"
-  ? "http://localhost:3001"
-  : window.location.origin;
-
-export const socket = io(serverUrl);
